1ff8caa9ddf3296d3c3547db6c8da27db1066c87
Kushwanth <89180481+Kushwanth88@users.noreply.github.com>
2024-04-10 20:17:43 +0530

    Updated graderepo file to find some data

--- src/main/java/com/cms/backend/repository/GradeRepository.java
+++ src/main/java/com/cms/backend/repository/GradeRepository.java
@@ -3,10 +3,12 @@ package com.cms.backend.repository;
 import com.cms.backend.model.GradeModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
 @Repository
 public interface GradeRepository extends JpaRepository<GradeModel, Integer> {
     List<GradeModel> findByTeacherId(Integer teacherId); // New method added
     List<GradeModel> findByStudentId(Integer studentId);
+    GradeModel findByAssignmentIdAndStudentId(Integer assignmentId, Integer studentId); // New method added
 }
